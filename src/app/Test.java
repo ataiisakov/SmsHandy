f6a906226eb209fb99b70d85b010107bebf33f32
ataiisakov <atayisakov558@gmail.com>
2020-10-31 19:14:56 +0000

    add files

---
+++ src/app/Test.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("javafx.runtime.version: " + System.getProperties().get("javafx.runtime.version"));
+        System.exit(0);
+    }
+    
+}
